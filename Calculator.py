0ea31d5886fd75e5a2063dc04c2bf856d7a73061
KwibooReese <reesehayman@gmail.com>
2024-01-19 10:58:15 +0000

    Improved Code, One function for calculating tax instead of 3

--- Calculator.py
+++ Calculator.py
@@ -1,34 +1,30 @@
-def calculate_tax_for_the_shire(grossPay):
-    # The friendly Shire has a 20% tax rate
-    return grossPay * 0.2
+regions_tax_rate = {"the_shire":0.2, "mirkwood":0.9, "mordor":0.9}
+
+def calculate_tax(gross_pay, region_string):
+    if region_string == "mordor":
+        tax = gross_pay * 0.9 + 500
+        return tax
+    else:
+        tax = gross_pay * regions_tax_rate.get(region_string)
+        return tax
 
 
-def calculate_tax_for_mirkwood(grossPay):
-    # Dodgy Mirkwood has a 90% tax rate
-    return grossPay * 0.9
-
-
-def calculate_tax_for_mordor(grossPay):
-    # Terrible Mordor has a 90% tax rate plus a fixed tax of £500.
-    return grossPay * 0.9 + 500
-
-
-def report_pay(gross_pay, calculate_tax):
+def report_pay(gross_pay, region_string):
     # The `calculate_tax` argument is a function
-    tax = calculate_tax(gross_pay)
+    tax = calculate_tax(gross_pay, region_string)
     net_pay = gross_pay - tax
     return f"Your gross pay was {gross_pay}, minus {tax} makes your net pay {net_pay}"
 
-
+print(f"{regions_tax_rate}\nThis shows the tax for each region")
 print("Frodo's Pay:")
-print(report_pay(5000.0, calculate_tax_for_the_shire))
+print(report_pay(5000.0, "the_shire"))
 # Your gross pay was 5000.0, minus 1000.0 makes your net pay 4000.0
 
 print("Bombadil's Pay:")
-print(report_pay(4320.0, calculate_tax_for_mirkwood))
+print(report_pay(4320.0, "mirkwood"))
 # Your gross pay was 4320.0, minus 3888.0 makes your net pay 432.0
 
 print("Mount Doom's Pay:")
-print(report_pay(5000.0, calculate_tax_for_mordor))
+print(report_pay(5000.0, "mordor"))
 # Your gross pay was 5000.0, minus 5000.0 makes your net pay 0.0
 
